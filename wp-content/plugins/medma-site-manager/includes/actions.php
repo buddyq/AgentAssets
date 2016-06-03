5b1057d181639ae3a120f2ef491dbd8a5b287bba
sagifire <myado@ukr.net>
2016-06-03 16:32:57 +0300

    fix order counter decrement

---
+++ wp-content/plugins/medma-site-manager/includes/actions.php
@@ -0,0 +1,19 @@
+<?php
+
+add_action( 'delete_blog', 'mism_blog_delete', 10, 2 );
+
+/**
+ * @param int $blog_id Blog ID
+ * @param bool $drop True if blog's table should be dropped. Default is false.
+ */
+function mism_blog_delete( $blog_id, $drop )
+{
+    if ($drop) {
+        /** @var wpdb */
+        global $wpdb;
+        $blogInfo = OrderMap::getBlogInfo($blog_id);
+        $wpdb->query('UPDATE `'.$wpdb->base_prefix . 'package_counter` SET `site_consumed` = `site_consumed` - 1 WHERE `id` = '.$blogInfo->counter_id);
+
+
+    }
+}

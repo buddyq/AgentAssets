18c9a4c77c4f9aca5863a63066bfeb344d00b299
aigletter <aigletter@gmail.com>
2016-08-03 00:01:10 +0300

    disable log paypal requests

--- wp-content/plugins/wp-paypal-ipn/includes/ipn.listener.class.php
+++ wp-content/plugins/wp-paypal-ipn/includes/ipn.listener.class.php
@@ -288,9 +288,11 @@ class IpnListener {
             throw new Exception("Invalid response status: ".$this->response_status);
         }
 
+        /*
         $logdata = "==================\n"."POST: ".var_export($this->post_data, true) . "\n\nSend data: ".$encoded_data."\n\n"
             ."response: " .$this->response . "\n\n";
         file_put_contents(dirname(__FILE__).'/responce.log', $logdata, FILE_APPEND);
+        */
         
         if (strpos($this->response, "VERIFIED") !== false) {
             return true;

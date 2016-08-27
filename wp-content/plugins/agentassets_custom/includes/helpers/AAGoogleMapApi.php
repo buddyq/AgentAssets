5a778c6b1e3ee5e1c014844d90f78e13beb25e3a
aigletter <aigletter@gmail.com>
2016-08-27 19:30:51 +0300

    add keyup handler

--- wp-content/plugins/agentassets_custom/includes/helpers/AAGoogleMapApi.php
+++ wp-content/plugins/agentassets_custom/includes/helpers/AAGoogleMapApi.php
@@ -105,6 +105,12 @@ class AAGoogleMapApi
                     };
                     
                     document.getElementById('direction_from_init').addEventListener('click', onChangeHandler);
+                    document.getElementById('direction_from_address').addEventListener('keyup', function(e){
+                        if (e.which == 13) {
+                            console.log(e.which);
+                            onChangeHandler();
+                        }
+                    });
                     
                     var marker = new google.maps.Marker({
                         position: " . json_encode($this->position) . ",

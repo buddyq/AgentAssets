84f52806c0199d74dd90a8aed4316b5bf7809c71
Buddy Quaid <bquaid@gmail.com>
2016-10-13 14:15:04 -0500

    Simplify My Groups output shortcode message.

--- wp-content/plugins/agentassets-site-manager/includes/shortcodes/medma_groups_info.php
+++ wp-content/plugins/agentassets-site-manager/includes/shortcodes/medma_groups_info.php
@@ -1,14 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 add_shortcode('aa_groups_info', 'aa_groups_info_shortcode');
 
 function aa_groups_info_shortcode(/*$atts*/) {
-=======
-add_shortcode('aa_groups_info', 'medma_groups_info_shortcode');
 
-function medma_groups_info_shortcode(/*$atts*/) {
->>>>>>> cbca85a547a01e619731d4a6c8e5344390fa2dc6
     //if (isset($_GET['form'])) return;
     global $wpdb;
     $groups = $wpdb->get_results('SELECT * FROM `'.MedmaGroupModel::tableName().'` mg INNER JOIN `'
@@ -19,10 +14,5 @@ function medma_groups_info_shortcode(/*$atts*/) {
         $result[] = $group->name;
     }
 
-<<<<<<< HEAD
     return count($result) ? (implode(', ',$result)) : 'You don\'t belong to any groups! :)';
 }
-=======
-    return count($result) ? 'You are a member of following groups: ' . (implode(', ',$result)).'<br/>' : '';
-}
->>>>>>> cbca85a547a01e619731d4a6c8e5344390fa2dc6

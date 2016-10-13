5ba331ce698916b6933e1c840ac623d122e72df2
Buddy Quaid <bquaid@gmail.com>
2016-10-13 10:52:30 -0500

    Edited group message for 'Your Groups' section in profile

--- wp-content/plugins/agentassets-site-manager/includes/shortcodes/medma_groups_info.php
+++ wp-content/plugins/agentassets-site-manager/includes/shortcodes/medma_groups_info.php
@@ -8,6 +8,7 @@ function aa_groups_info_shortcode(/*$atts*/) {
     $groups = $wpdb->get_results('SELECT * FROM `'.MedmaGroupModel::tableName().'` mg INNER JOIN `'
         .$wpdb->base_prefix.'medma_group_user` mgu ON mg.id = mgu.group_id WHERE mgu.user_id = '.(int)get_current_user_id());
 
+
     $result = array();
     if (false !== $groups) foreach($groups as $group) {
         $result[] = $group->name;

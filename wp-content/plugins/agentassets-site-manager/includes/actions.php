4c31b4e8edf86906a3a805758dd298d3f066c30d
Buddy Quaid <bquaid@gmail.com>
2016-10-20 11:29:07 -0500

    Fixed actions with query var

--- wp-content/plugins/agentassets-site-manager/includes/actions.php
+++ wp-content/plugins/agentassets-site-manager/includes/actions.php
@@ -1,9 +1,16 @@
 <?php
+add_action( 'delete_blog', 'mism_blog_delete', 10, 2 );
+
+//Detect from cronjob in cPanel to check for expired sanitize_comment_cookies
+if (isset($_REQUEST['check_expired'])) {
+  if ($_REQUEST['check_expired'] == 1) {
+    include_once('ajax_action_callbacks.php');
+    do_action('wp_ajax_check_sites_for_removing');
+  }
+}
 include_once('ajax_action_callbacks.php');
 
 do_action('wp_ajax_check_sites_for_removing');
-
-add_action( 'delete_blog', 'mism_blog_delete', 10, 2 );
 
 /**
  * @param int $blog_id Blog ID
